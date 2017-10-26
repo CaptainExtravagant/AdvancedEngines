bd06bf0e63c309ee5f06e6c1be6e65bd2b1de5a6
CaptainExtravagant <32903246+CaptainExtravagant@users.noreply.github.com>
2017-10-26 12:57:43 +0100

    Revert "Quite a Bit"
    This reverts commit c9aaef5599d32275555f1b23f9f32785d1d2ac09.

--- Source/ContentExamples/ContentExamples.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ContentExamples.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ContentExamples, "ContentExamples" );
